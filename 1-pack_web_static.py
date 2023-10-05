ed347f452107b47b3c1d438adc2539ad4b9657ce
Mwmuraguri <maryannewanjiku87@gmail.com>
2023-10-05 05:53:58 -0700

    Script that generates a .tgz archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -13,8 +13,8 @@ def do_pack():
     local("mkdir -p versions")
     path = ("versions/web_static_{}.tgz"
             .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")))
-    result = local("tar -cvzf {} web_static"
-                   .format(path))
+
+    result = local("tar -cvzf {} -C web_static .".format(path))
 
     if result.failed:
         return None
